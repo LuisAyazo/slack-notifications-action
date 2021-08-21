733809226971561bd8cb90d2d6ec71b045170b34
Luis Ayazo <luis.ayazoh@gmail.com>
2021-08-21 03:16:04 -0500

    add python script

--- script.py
+++ script.py
@@ -1,30 +1,89 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
-import json
+from slack import WebClient
+from slack.errors import SlackApiError
 
-def main():
-    my_input = os.environ["INPUT_CHANNEL"]
-    webhook = os.environ["SLACK_WEBHOOK"]
-    
-    print(f" WEBHOOK FROM SLACK: {webhook}")
+default_channel = "devops-notifications"
 
-    message_id = f"Hello {my_input}"
-    print(f"probando esto estamos {my_input}")
-    print(f"::set-output name=message_id::{webhook}")
+if not "SLACK_API_TOKEN" in os.environ:
+    raise Exception('SLACK_API_TOKEN env is missing. Please add it to this action for proper execution.')
+slack_token = os.environ["SLACK_API_TOKEN"]
 
-    webhook_url = webhook
-    slack_data = {'text': "Sup! Test from custom github action :spaghetti:"}
+if not "INPUT_DEPLOYMENT_URL" in os.environ:
+    raise Exception('deployment_url is missing. check if deployment was executed succefully')
+cloud_run_deployment_url = os.environ["INPUT_DEPLOYMENT_URL"]
 
-    response = requests.post(
-        webhook_url, data=json.dumps(slack_data),
-        headers={'Content-Type': 'application/json'}
-    )
-    if response.status_code != 200:
-        raise ValueError(
-            'Request to slack returned an error %s, the response is:\n%s'
-            % (response.status_code, response.text)
-        )
-    
+if not "SLACK_USER_ID" in os.environ:
+  channel = os.getenv('INPUT_CHANNEL', default_channel)
+else:
+  channel = os.environ["SLACK_USER_ID"]
 
-if __name__ == "__main__":
-    main()
+color = os.environ["INPUT_COLOR"]
+print(color)
+# slack_token = os.environ["INPUT_CHANNEL_ID"]
+# slack_token = os.environ[""]
+# slack_token = os.environ["SLACK_API_TOKEN"]
+# slack_token = os.environ["SLACK_API_TOKEN"]
+
+# repo_github_action_url = os.environ["GITHUB_SERVER_URL"] + "/" + os.environ["GITHUB_SERVER_URL"] + "/commit/" + os.environ["GITHUB_SHA"]  + "/checks|" + os.Getenv("GITHUB_WORKFLOW")
+
+# print(repo_github_action_url)
+
+client = WebClient(token=slack_token)
+
+message_attachments = [
+        {
+	        "mrkdwn_in": ["text"],
+            "color": "#36a64f",
+            "pretext": "_Deployment created for_ *CLIENT-WEB* _branch:_ `v0.0.1` ",
+            "author_name": "author_name",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "https://placeimg.com/16/16/people",
+            "title": "Link to..",
+            "title_link": "https://api.slack.com/",
+            # "title": "other",
+            "text": "Optional `text` that appears within the attachment",
+            "fields": [
+                                {
+                    "title": "Cloud Run deploy URL",
+                    "value": "<http://i.imgur.com/nwo13SM.png|Link to cloud run>",
+                    "short": True
+                },
+                {
+                    "title": "Actions URL",
+                    "value": "<http://i.imgur.com/nwo13SM.png|Link to github action>",
+                    "short": True
+                },
+                {
+                    "title": "Commit",
+                    "value": "<http://i.imgur.com/nwo13SM.png|Commit>",
+                    "short": True
+                },
+                {
+                    "title": "Status",
+                    "value": "SUCCESS",
+                    "short": True
+                }
+            ],
+            # "thumb_url": "http://placekitten.com/g/200/200",
+            "footer": "Created by DevOps | Infra Team",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            # "ts": 123456789
+        }
+    ]
+
+try:
+  # if !channel
+  response = client.chat_postMessage(
+    channel=channel,
+    text="Hello from your app! :tada:",
+    attachments=message_attachments
+  )
+
+  # response = client.conversations_open(users=["UEQM4T18W"])
+except SlackApiError as e:
+  # You will get a SlackApiError if "ok" is False
+  assert e.response["error"] 
+
