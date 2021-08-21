b4e050a7bceb5f7f0c282ba659c60da1427a6da7
Luis Ayazo <luis.ayazoh@gmail.com>
2021-08-20 23:39:05 -0500

    add python script

--- script.py
+++ script.py
@@ -12,13 +12,19 @@ def main():
     print(f"probando esto estamos {my_input}")
     print(f"::set-output name=message_id::{webhook}")
 
+    webhook_url = webhook
+    slack_data = {'text': "Sup! Test from custom github action :spaghetti:"}
 
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+    raise ValueError(
+        'Request to slack returned an error %s, the response is:\n%s'
+        % (response.status_code, response.text)
+    )
     
-    myobj = {'text': 'Test from custom github action'}
-
-    x = requests.post(webhook, data = myobj)
-
-    print(x.text)
 
 if __name__ == "__main__":
     main()
