509be52e44da22645b89083d068d444d52d24b10
Luis Ayazo <luis.ayazoh@gmail.com>
2021-08-23 03:04:54 -0500

    add new test

--- script.py
+++ script.py
@@ -4,10 +4,12 @@ logging.basicConfig(level=logging.DEBUG)
 import os
 from slack import WebClient
 from slack.errors import SlackApiError
+from jinja2 import Template
 
 # Github block
 github_job = os.environ["GITHUB_JOB"]
 github_user = os.environ["GITHUB_ACTOR"] 
+github_user_url = f"https://github.com/{ github_user }"
 github_event = os.environ["GITHUB_EVENT_NAME"]
 github_repo_name = os.environ["GITHUB_REPOSITORY"]
 github_branch_tag = '/'.join(os.environ["GITHUB_REF"].split("/")[2:])
@@ -22,6 +24,10 @@ job_message = ""
 job_color = ""
 
 # Input Vars
+channel                     =  os.getenv('INPUT_CHANNEL', default_channel)
+message_id                  =  os.getenv("INPUT_MESSAGE_ID", '')
+job_status                  =  os.getenv("INPUT_JOB_STATUS", '')
+cloud_run_deployment_status =  os.getenv('INPUT_DEPLOYMENT_STATUS', '')
 if not "SLACK_API_TOKEN" in os.environ:
     raise Exception('SLACK_API_TOKEN env is missing. Please add it to this action for proper execution.')
 slack_token = os.environ["SLACK_API_TOKEN"]
@@ -30,41 +36,63 @@ if not "INPUT_DEPLOYMENT_URL" in os.environ:
     raise Exception('deployment_url is missing. check if deployment was executed succefully')
 cloud_run_deployment_url = os.environ["INPUT_DEPLOYMENT_URL"]
 
-if not "SLACK_USER_ID" in os.environ:
-  channel = os.getenv('INPUT_CHANNEL', default_channel)
-else:
-  channel = os.environ["SLACK_USER_ID"]
 
-if 'INPUT_MESSAGE_ID' in os.environ:
-    message_id = os.environ["INPUT_MESSAGE_ID"]
+# if not "SLACK_USER_ID" in os.environ:
+#   channel = os.getenv('INPUT_CHANNEL', default_channel)
+# else:
+#   channel = os.environ["SLACK_USER_ID"]
 
-
-def test():
-  return '"text": ":warning::warning: `Errror in github actions` :warning::warning:",'
 # Colors block
-status = os.environ['DEPLOYMENT_STATUS']
-job_status = os.environ["INPUT_JOB_STATUS"]
-print(f"Job Status: {job_status}")
 
-if job_status == "success" and status == "True":
-    deployment_color = "#26C10A"
+if cloud_run_deployment_status == "":
+    deployment_message = "UNKNOWN"
+
+if job_status == "success" and cloud_run_deployment_status == "True":
+    github_job_message = ":white_check_mark: *_Github Action finished succefully_* :white_check_mark:"
+    deployment_color = "#0EB70B"
     deployment_message = "SUCCESS"
-
-elif job_status == "failure" and status == "True":
-   # Job is failure in some step
-   job_attachment = "jobATTCH"
-   job_message = "Github Action Failure"
-   job_color = "#FF0000"
-   def test():
-     return '"text": ":warning::warning: `Errror in github actions` :warning::warning:",'
- 
-   # deployment to cloud run is success
-   deployment_color = "#26C10A"
-   deployment_message = "SUCCESS"
+elif job_status == "success" and cloud_run_deployment_status == "False":
+    github_job_message = ":white_check_mark: *_Github Action finished succefully_* :white_check_mark:"
+    deployment_message = "FAILURE"
+    deployment_color = "#DE1919"
+elif job_status == "failure" and cloud_run_deployment_status == "True":
+    github_job_message = ":warning::warning: *_Github Action finished with failure_* :warning::warning:"
+    deployment_color = "#0EB70B"
+    deployment_message = "SUCCESS"
+elif job_status == "failure" and cloud_run_deployment_status == "False":
+    github_job_message = ":warning::warning: *_Github Action finished with failure_* :warning::warning:"
+    deployment_color = "#DE1919"
+    deployment_message = "FAILURE"
+elif job_status == "cancelled" and cloud_run_deployment_status == "True":
+    github_job_message = ":information_source: *_~Github Action has been cancelled~_* :information_source:"
+    deployment_color = "#0EB70B"
+    deployment_message = "SUCCESS"
+elif job_status == "cancelled" and cloud_run_deployment_status == "False":
+    github_job_message = ":information_source: *_~Github Action has been cancelled~_* :information_source:"
+    deployment_color = "#DE1919"
+    deployment_message = "FAILURE"
 else:
-   #  job_status = cancelled
-   deployment_color = "#FF0000"
-   deployment_message = "FAILURE"
+    github_job_message = f":sos: *_Github Action status:_* { job_status } :sos:" 
+    deployment_color = "#D3D3D3"
+    deployment_message = "UNKNOWN: CHECK THE URL"
+
+
+# data fot the template
+data = {
+    "github_repo_name": os.environ["GITHUB_REPOSITORY"],
+    "github_branch_tag": '/'.join(os.environ["GITHUB_REF"].split("/")[2:]),
+    "github_user": github_user,
+    "github_user_url": github_user_url,
+    "github_action_url": os.environ["GITHUB_SERVER_URL"] + "/" + os.environ["GITHUB_REPOSITORY"] + "/commit/" + os.environ["GITHUB_SHA"]  + "/checks" ,
+    "github_wokflow": os.environ["GITHUB_WORKFLOW"],
+    "github_commit": os.environ["GITHUB_SERVER_URL"] + "/" + os.environ["GITHUB_REPOSITORY"] + "/commit/" + os.environ["GITHUB_SHA"],
+    "github_job_message": github_job_message,
+    "cloud_run_deployment_url": cloud_run_deployment_url,
+    "deployment_message": deployment_message,
+    "deployment_color": deployment_color,
+    "footer_message": "Created by DevOps | Infra Team",
+    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png"
+}
                 
 # Prints Block
 print(f"j_message: { job_message }")
@@ -78,49 +106,53 @@ print(f"GITHUB_ACTOR: { github_user }")
 print(f"GITHUB_EVENT_NAME: { github_event}")
 print(f"GITHUB_REF: { github_branch_tag }")
 
-
-
 # Slack Block
 client = WebClient(token=slack_token)
-message_attachments = [
+template = """
+[
         {
 	        "mrkdwn_in": ["text"],
-            "color": f"{deployment_color}",
-            "pretext": f"_Deployment created for_ *{ github_repo_name }* _ref:_ `{ github_branch_tag}` ",
-            "author_name": f"{ github_user }",
-            "author_link": "http://flickr.com/bobby/",
+            # default color if deployment_color is not defined
+            "color": "{{ deployment_color | default('#FF0000')}}",
+            "pretext": "_Deployment created for_ *{{ github_repo_name }}* _ref:_ `{{ github_branch_tag }}` ",
+            "author_name": "{{ github_user }}", 
+            "author_link": "{{ github_user_url }}",
             "author_icon": "https://placeimg.com/16/16/people",
-            # "title": "Link to..",
             # "title_link": "https://api.slack.com/",
-            print(test())
+            "text": "{{ github_job_message | default() }}",
             "fields": [
-                {
-                    "title": "Cloud Run deploy URL",
-                    "value": f"<{ cloud_run_deployment_url }|Link to cloud run>",
-                    "short": True
-                },
+
                 {
                     "title": "Actions URL",
-                    "value": f"<{ github_action_url }|Github action: { github_wokflow }>",
+                    "value": "<{{ github_action_url }}|Github action: {{ github_wokflow }}>",
                     "short": True
                 },
                 {
                     "title": "Commit",
-                    "value": f"<{ github_commit }|Commit>",
+                    "value": "<{{ github_commit }}|Commit>",
                     "short": True
                 },
                 {
-                    "title": "Status",
-                    "value": f"{deployment_message}",
+                    "title": "Cloud Run deploy URL",
+                    "value": "<{{ cloud_run_deployment_url | default('')}}|Link to cloud run>",
+                    "short": True
+                },
+                {
+                    "title": "Cloud run deployment",
+                    "value": "`{{ deployment_message }}`",
                     "short": True
                 }
             ],
             # "thumb_url": "http://placekitten.com/g/200/200",
-            "footer": "Created by DevOps | Infra Team",
-            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-            # "ts": 123456789
+            "footer": "{{ footer_message }}",
+            "footer_icon": " {{ footer_icon }}",
         }
     ]
+"""
+
+
+json_data = Template(template)
+message_attachments = json_data.render(data)
 
 try:
   if not message_id:
